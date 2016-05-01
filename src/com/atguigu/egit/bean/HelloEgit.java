fd1d045161849ece1f47c1a1d54e305c2918634d
x-zhq <x-zhq@163.com>
2016-05-01 14:04:44 +0800

    大领导说话了

--- src/com/atguigu/egit/bean/HelloEgit.java
+++ src/com/atguigu/egit/bean/HelloEgit.java
@@ -7,12 +7,16 @@ public class HelloEgit {
 		System.out.println("update and ==");
 		System.out.println("update01!");
 		System.out.println("update02!");
+		
+		System.out.println("我可是大领导，你小子小心点");
 
 
 		System.out.println("我在这里做了修改了，：）");
 		System.out.println("要和你冲突。。。。中文乱码");
 
 		System.out.println("这个还会中文乱码吗？");
+
+		System.out.println("我可是大领导，你小子小心点");	
 
 	}
 }
